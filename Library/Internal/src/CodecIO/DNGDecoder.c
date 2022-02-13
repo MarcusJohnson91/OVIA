fcf8dbd2585574692fae4900b41f314d5905bf7f
Marcus Johnson <MarcusLJohnson1991@gmail.com>
2022-02-12 23:46:34 -0500

    DNG is a subset of TIFF, deleted from CodecIO

--- Library/Internal/src/CodecIO/DNGDecoder.c
+++
@@ -1,9 +0,0 @@
-#include "../../include/CodecIO/DNGCodec.h"
-
-#if (PlatformIO_Language == PlatformIO_LanguageIsCXX)
-extern "C" {
-#endif
-
-#if (PlatformIO_Language == PlatformIO_LanguageIsCXX)
-}
-#endif
